264dafe3a50748825952c262a8f581acb731fbd4
Mike Mills <mmills@geoengineers.com>
2014-04-09 10:50:22 -0700

    Area Stats Update

--- scripts/appClient/src/extensions/washington_stats.js
+++ scripts/appClient/src/extensions/washington_stats.js
@@ -2,23 +2,19 @@ BootstrapVars = {} || BootstrapVars;
 
 BootstrapVars.areaStats = [
 	{
-  		agency: "Washington Department of Natural Resources",
-  		total_acres: "912984.589506",
-  		mapUrl: "http://{s}.tiles.mapbox.com/v3/smartmine.izm5nrk9/"
+  		agency: "Department of Fish & Wildlife",
+  		total_acres : "912984.589506",
+      total_cost: "123456934.00",
+      total_revenue: "234456934.00",
+  		mapUrl: "http://{s}.tiles.mapbox.com/v3/smartmine.izm5nrk9/",
+      icon: "dfw.png"
 	},
 	{
-  		agency: "Washington Department of Fish & Wildlife",
-  		total_acres : "912984.589506",
-  		mapUrl: "http://{s}.tiles.mapbox.com/v3/smartmine.izm5nrk9/"
-	},
-	{
-  		agency: "Washington State Parks",
-  		total_acres : "912984.589506",
-  		mapUrl: "http://{s}.tiles.mapbox.com/v3/smartmine.izm5nrk9/"
-	},
-	{
-  		agency: "Washington Department of Recreation & Conservation",
-  		total_acres : "912984.589506",
-  		mapUrl: "http://{s}.tiles.mapbox.com/v3/smartmine.izm5nrk9/"
+  		agency: "Bureau of Land Managment",
+  		total_acres : "2912984.589506",
+      total_cost: "567456934.00",
+      total_revenue: "435456934.00",
+  		mapUrl: "http://{s}.tiles.mapbox.com/v3/smartmine.1cqv9529/"
+      icon: "blm.png"
 	}
 ];
