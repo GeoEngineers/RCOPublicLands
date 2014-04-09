ad0b71db88b4fbbc6db03af7655e0f0bc374e64f
JDStopchick <JStopchick@geoengineers.com>
2014-04-09 11:44:44 -0700

    fixed broken stats file

--- scripts/appClient/src/extensions/washington_stats.js
+++ scripts/appClient/src/extensions/washington_stats.js
@@ -2,19 +2,19 @@ BootstrapVars = {} || BootstrapVars;
 
 BootstrapVars.areaStats = [
 	{
-  		agency: "Department of Fish & Wildlife",
-  		total_acres : "912984.589506",
-      total_cost: "123456934.00",
-      total_revenue: "234456934.00",
-  		mapUrl: "http://{s}.tiles.mapbox.com/v3/smartmine.izm5nrk9/",
-      icon: "dfw.png"
+		agency: "Department of Fish & Wildlife",
+		total_acres : "912984.589506",
+		total_cost: "123456934.00",
+		total_revenue: "234456934.00",
+		mapUrl: "http://{s}.tiles.mapbox.com/v3/smartmine.izm5nrk9/",
+		icon: "dfw.png"
 	},
 	{
-  		agency: "Bureau of Land Managment",
-  		total_acres : "2912984.589506",
-      total_cost: "567456934.00",
-      total_revenue: "435456934.00",
-  		mapUrl: "http://{s}.tiles.mapbox.com/v3/smartmine.yyb3ayvi/"
-      icon: "blm.png"
+		agency: "Bureau of Land Managment",
+		total_acres : "2912984.589506",
+		total_cost: "567456934.00",
+		total_revenue: "435456934.00",
+		mapUrl: "http://{s}.tiles.mapbox.com/v3/smartmine.yyb3ayvi/",
+		icon: "blm.png"
 	}
 ];
