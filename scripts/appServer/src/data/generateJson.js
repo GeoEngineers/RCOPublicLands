97113f273303a85a471f09c6fdb90d504df6321e
Mike Mills <mmills@geoengineers.com>
2014-07-02 12:14:14 -0700

    generate json update

--- scripts/appServer/src/data/generateJson.js
+++ scripts/appServer/src/data/generateJson.js
@@ -1,12 +1,62 @@
 
 var fs = require("fs");
 var data = [];
+
+var targetType = 'statewide'; // Change this to update the specific sums json file
+
+var targetData = [
+{
+	name: 'congressional',
+	csvTarget: './export_cong.csv',
+	fileName: 'sums_congressional_districts.json',
+	variableName: 'sums_congressional_districts'
+},
+{
+	name: 'counties',
+	csvTarget: './export_county.csv',
+	fileName: 'sums_counties.json',
+	variableName: 'sums_counties'
+},
+{
+	name: 'legislative',
+	csvTarget: './export_leg.csv',
+	fileName: 'sums_legdistricts.json',
+	variableName: 'sums_legdistricts'
+},
+{
+	name: 'wrias',
+	csvTarget: './export_wria.csv',
+	fileName: 'sums_wrias.json',
+	variableName: 'sums_wria'
+},
+{
+	name: 'statewide',
+	csvTarget: './sums_statewide.csv',
+	fileName: 'sums_statewide.json',
+	variableName: 'sums_statewide'
+}
+];
+
+var filesLocation = '../../../appClient/src/extensions/';
+var csvTarget = ''
+var fileName = ''
+var variableName = '';
+
+
 function generateJson(){
-    //fs.readFile('./export_cong.csv', 'utf8', function (err,dataset) {
-	//fs.readFile('./export_county.csv', 'utf8', function (err,dataset) {
-	//fs.readFile('./export_leg.csv', 'utf8', function (err,dataset) {
-	fs.readFile('./export_wria.csv', 'utf8', function (err,dataset) {
-	//fs.readFile('./sums_statewide.csv', 'utf8', function (err,dataset) {
+
+	//Get Settings for the files
+	for(var i=0; i < targetData.length; i++)
+	{
+		if(targetData[i].name === targetType)
+		{
+			csvTarget = targetData[i].csvTarget;
+			fileName = targetData[i].fileName;
+			variableName = targetData[i].variableName;
+		}
+
+	}
+	fs.readFile(csvTarget, 'utf8', function (err,dataset) {
 	  	if (err) {
 	    	console.log(err);
 	  	}
@@ -17,25 +67,6 @@ function generateJson(){
 		var count = 0;
 		var rowcount;
 	  	
-	  	//console.log(data);
-	  	console.log("---------------------------------------------------------------------");
-	  	console.log("---------------------------------------------------------------------");
-	  	console.log("---------------------------------------------------------------------");
-	  	console.log("---------------------------------------------------------------------");
-	  	console.log("---------------------------------------------------------------------");
-	  	console.log("---------------------------------------------------------------------");
-	  	console.log("---------------------------------------------------------------------");
-	  	console.log("---------------------------------------------------------------------");
-	  	console.log("---------------------------------------------------------------------");
-	  	console.log("---------------------------------------------------------------------");
-	  	console.log("---------------------------------------------------------------------");
-	  	console.log("---------------------------------------------------------------------");
-	  	console.log("---------------------------------------------------------------------");
-	  	console.log("---------------------------------------------------------------------");
-	  	console.log("---------------------------------------------------------------------");
-	  	console.log("---------------------------------------------------------------------");
-	  	console.log("---------------------------------------------------------------------");
-	  	console.log("---------------------------------------------------------------------");
 	  	
 	  	var values = [];
 		while (index > -1) {
@@ -45,10 +76,15 @@ function generateJson(){
 	      if(count > 0)
 	      {
 	      	var splitData = line.split('|');
-	      	var value = {"acres": parseFloat(splitData[0]), "acquisitioncost": parseFloat(splitData[1]),"name": splitData[2].replace('\r', '').replace('"', '').replace('"', ''), "agency": splitData[3].replace('\r', '').replace('"', '').replace('"', '')};
-	      	//Use this line if generating statewide totals
-	      	//var value = {"acres": parseFloat(splitData[0]), "acquisitioncost": parseFloat(splitData[1]), "agency": splitData[2].replace('\r', '').replace('"', '').replace('"', '')};
-	      	
+	      	var value = {};
+	      	if(targetType !== 'statewide')
+	      	{
+	      		value = {"acres": parseFloat(splitData[0]), "acquisitioncost": parseFloat(splitData[1]),"name": splitData[2].replace('\r', '').replace('"', '').replace('"', ''), "agency": splitData[3].replace('\r', '').replace('"', '').replace('"', '')};
+	      	}
+	      	else
+	      	{
+	      		value = {"acres": parseFloat(splitData[0]), "acquisitioncost": parseFloat(splitData[1]), "agency": splitData[2].replace('\r', '').replace('"', '').replace('"', '')};
+	      	}
 	      	values.push(value);
 	  	  }
 	  	  else
@@ -57,7 +93,17 @@ function generateJson(){
 	  	  }
 	      count++;
 	    }
-	    console.log(JSON.stringify(values));
+
+	    var newJson = "var " + variableName + "= " + JSON.stringify(values) + "; //congressional districts update!";
+	    //console.log(newJson);
+
+	    fs.writeFile(filesLocation + fileName, newJson, function(err) {
+	    		if(err) {
+	        		console.log(err);
+	    		} else {
+	        		console.log("The file was saved!");
+	    		}
+		}); 
 	});
 
 
