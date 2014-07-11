d17238953ad38530a83d5e9e9ed0003772c6295c
Jonathan Stopchick <jstopchick@geoengineers.com>
2014-07-11 16:00:47 -0700

    tracking

--- scripts/appClient/src/extensions/analyticsWrapper.js
+++ scripts/appClient/src/extensions/analyticsWrapper.js
@@ -6,8 +6,8 @@ var GA = {};
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-50069705-1', 'publiclands.smartmine.com');
-
+ga('create', 'UA-50069705-1', 'auto');
+  
 _.extend(GA, {
 	logGAPageView: function(){
 		ga('send', 'pageview');
